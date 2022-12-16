bf0259092a37597f3e175bd2f4f16e3cc1c037ab
Herbertoliveira <herbertoliveiraoficial@gmail.com>
2022-12-16 00:11:04 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1>Agência Bancária </h1> 
+
+<p>Bem Vindos A Nossa Agência</p>
+<p align="center">
+
+<h3>Features</h3>
+
+- [x] Opção 1 - Criar conta <br>
+- [x] Opção 2 - Depositar<br>
+- [x] Opção 3 - Sacar<br>
+- [x] Opção 4 - Transferir<br>
+- [x] Opção 5 - Listar<br> 
+- [x] Opção 6 - Sair<br>
---
+++ src/utils/Util.java
@@ -0,0 +1,23 @@
+package utils;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Util {
+    
+    static NumberFormat formatandoNumeros = new DecimalFormat(" R$ #,##0.00"); 
+
+    static SimpleDateFormat formatandoData = new SimpleDateFormat("dd/MM/yyyy");
+
+    public static String dateToString(Date data) {
+        return Util.formatandoData.format(data);
+    }
+
+    public static String doubleToString(Double valor) {
+        return Util.formatandoNumeros.format(valor);
+    }
+
+
+}
